ae2b4753121f193b3b5639f1560156312976b864
Anthony <anthshor@gmail.com>
2022-06-30 22:56:37 +1200

    Update org

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "shorter-solutions"
 
     workspaces {
       name = "learn-terraform-pipelines-consul"
@@ -17,7 +17,7 @@ terraform {
     }
   }
 
-  required_version = "~> 0.14"
+  required_version = ">= 1.1.0"
 }
 
 
